2d5c0bb431821cb627408beb87b2d3f2b979b861
Lexie Gaines-Smith <againessmith@gmail.com>
2025-03-06 23:43:26 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@
 import { HeroUIProvider } from "@heroui/react";
 import dynamic from "next/dynamic";
 import { Toaster } from "react-hot-toast";
-import BackToTopButton from "./components/BackToTopButton";
-import PortfolioNavBar from "./components/PortfolioNav";
 export default function Home() {
   const PortfolioContent = dynamic(
     () => import("./components/PortfolioContent"),
     { ssr: false }
   );
+  const PortfolioNavBar = dynamic(
+    () => import("./components/PortfolioNav"),
+    { ssr: false }
+  );
+  const BackToTopButton = dynamic(() => import("./components/BackToTopButton"), { ssr: false});
   return (
     <HeroUIProvider>
       <PortfolioNavBar></PortfolioNavBar>
